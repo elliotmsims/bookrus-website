c8f1011af1d41d592aa996cf1c39672d59b90cfc
Hrithikr <55152983+Hrithikr@users.noreply.github.com>
2022-03-02 17:32:37 -0600

    added to the about page

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -5,7 +5,12 @@ export default function About() {
     return(
         <>
         <Container>
-            
+            <h3>Objective:</h3>
+            <p>BooksRUs exists to emphasize the literary contributions of different cultures around the world. The app will include information
+regarding famous publications around the globe, their authors, and the countries they hale from. The vision of BooksRUs is to connect
+and spread knowledge from distant lifestyles and faraway populations to others through sharing inspiring stories, insightful biographies,
+and cross-country cultural exchange. This is because, ultimately, the more learned we are of each other, the more mindful we are of
+the ways the world work.</p>
         </Container>
         </>
     )
