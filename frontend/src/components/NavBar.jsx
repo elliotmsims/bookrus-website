d0a5ad95ae2033aa71972430e9131311ff774026
Francis0312 <pacotron2001@gmail.com>
2022-03-03 15:45:49 -0600

    Add logo to nav, edit nav hover, change font size

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -2,12 +2,21 @@ import React from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import "./Navbar.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import logo from "../assets/logo.png";
 
 export default function Navigation() {
   return (
-    <Navbar className="nav-color" variant="dark">
+    <Navbar className="nav-color" variant="dark" expand="lg">
       <Container>
-        <Navbar.Brand href="/">Home</Navbar.Brand>
+        <Navbar.Brand href="/">
+          <img
+            src= {logo}
+            width="190"
+            height="60"
+            className="d-inline-block align-top"
+            alt="React Bootstrap logo"
+          />
+        </Navbar.Brand>
         <Nav className="me-auto">
           <Nav.Link href="/about">About</Nav.Link>
           <Nav.Link href="/books">Books</Nav.Link>
