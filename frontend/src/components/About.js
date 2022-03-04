33f9de3890f0503096b2408067573371f0fc5294
Hrithikr <55152983+Hrithikr@users.noreply.github.com>
2022-03-04 14:30:16 -0600

    trying to get the latest:

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -2,6 +2,9 @@ import { Container } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function About() {
+    
+
+
     return(
         <>
         <Container>
