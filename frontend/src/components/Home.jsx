e0d3bbd1e9facddb5dcd55f6a2312f991469023c
elliotsims <elliotmsims@utexas.edu>
2022-03-03 18:44:24 -0600

    new favicon

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { Container } from "react-bootstrap";
-import "./Home.css"
+// import { Container } from "react-bootstrap";
+import "./Home.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function Home() {
@@ -8,8 +8,8 @@ export default function Home() {
     <div className="main">
       <div
         className="home-bg"
-        style={{ backgroundImage: `url('splash-art.jpg')` }}>
-      </div>
+        style={{ backgroundImage: `url('splash-art.jpg')` }}
+      />
     </div>
   );
 }
