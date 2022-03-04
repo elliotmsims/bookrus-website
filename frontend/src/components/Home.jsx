71c1166fdb4221a1f993ee641fb4a459075400c4
Francis0312 <pacotron2001@gmail.com>
2022-03-04 03:28:39 -0600

    Put card in correct place on home screen!

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,6 +1,15 @@
 import React from "react";
 import "./Home.css";
+import {
+  Container,
+  Card,
+  Row,
+  Col,
+  ListGroup,
+  ListGroupItem,
+} from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Typography from "@mui/material/Typography";
 
 export default function Home() {
   return (
@@ -9,7 +18,29 @@ export default function Home() {
         className="home-bg"
         style={{ backgroundImage: `url('splash-art.jpg')` }}
       />
-      <div className="bottom" />
+      <div className="bottom">
+        <Container style={{ maxWidth: "100%" }}>
+          <div className="bg-text">
+            Test Header
+          </div>
+          <Row
+            md={3}
+            className="dev_cards"
+            style={{ justifyContent: "space-evenly" }}
+          >
+            <Col>
+              <Card>
+                <Card.Img variant="top" src="holder.js/100px160" />
+                <Card.Body>
+                  <Card.Title>name</Card.Title>
+                  <Card.Subtitle className="text-muted">subtitle</Card.Subtitle>
+                  <Card.Text>description</Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
+      </div>
     </div>
   );
 }
