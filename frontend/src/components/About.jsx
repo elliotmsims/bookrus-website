1dcd6f273be0ed4929818f7357a3ae47cfa2b0d1
William Eng <williame007@gmail.com>
2022-03-03 23:49:20 -0600

    Begin adding dev cards to About

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -1,7 +1,38 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Container, Card, Row, Col, ListGroup, ListGroupItem } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+const names = {
+  devs: [
+    {
+      name: "Matthew Escobar",
+      description: "test 1",
+      gitlab_id: "Matt594",
+    },
+    {
+      name: "Hrithik Ramganesh",
+      description: "test 2",
+      gitlab_id: "Hrithikr",
+    },
+    {
+      name: "Elliot Sims",
+      description: "test 3",
+      gitlab_id: "elliotsims",
+    },
+    {
+      name: "Francisco Reyna",
+      description: "test 4",
+      gitlab_id: "francis0312",
+    },
+    {
+      name: "William Eng",
+      description: "test 5",
+      gitlab_id: "willeng37",
+    },
+  ],
+};
+
+// About page
 export default function About() {
   return (
     <Container>
@@ -17,6 +48,30 @@ export default function About() {
         learned we are of each other, the more mindful we are of the ways the
         world work.
       </p>
+      <Row
+        md={3}
+        className="dev_cards"
+        style={{ justifyContent: "space-evenly" }}>
+        {names.devs.map((dev) => (
+          <Col>
+            <Card>
+              <Card.Img variant="top" src="holder.js/100px160" />
+              <Card.Body>
+                <Card.Title>{dev.name}</Card.Title>
+                <Card.Subtitle className="text-muted">
+                  {dev.gitlab_id}
+                </Card.Subtitle>
+                <Card.Text>{dev.description}</Card.Text>
+              </Card.Body>
+              <ListGroup>
+                <ListGroupItem>Commits: </ListGroupItem>
+                <ListGroupItem>Issues: </ListGroupItem>
+                <ListGroupItem>Tests: </ListGroupItem>
+              </ListGroup>
+            </Card>
+          </Col>
+        ))}
+      </Row>
     </Container>
   );
 }
