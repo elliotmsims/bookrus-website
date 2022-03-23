57103e2a08cdb82570c274ab37810354aab56dd4
William Eng <williame007@gmail.com>
2022-03-23 00:10:29 -0500

    Updated app.py to return an API using our database of the two currently available models

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,52 @@
 from flask import Flask
+from flask_restless import APIManager
+from flask_sqlalchemy import SQLAlchemy
+import credentials
 
 app = Flask(__name__)
+app.debug = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = credentials.db_login
+db = SQLAlchemy(app)
+
+# Define Book table/data model
+class Book(db.Model):
+    book_id = db.Column(db.Integer, primary_key=True)
+    book_title = db.Column(db.String())
+    book_author = db.Column(db.String())
+    book_published = db.Column(db.String())
+    book_pages = db.Column(db.Integer)
+    book_maturity = db.Column(db.String())
+    book_language = db.Column(db.String())
+    book_categories = db.Column(db.String())
+    book_description = db.Column(db.String())
+
+# Define Country table/data model
+class Country(db.Model):
+    country_id = db.Column(db.Integer, primary_key=True)
+    country_name = db.Column(db.String())
+    country_region = db.Column(db.String())
+    country_capital_city = db.Column(db.String())
+    country_lat = db.Column(db.Float)
+    country_long = db.Column(db.Float)
+
+# Build database
+db.create_all()
+
+# Define database API system here, accessible at /api/[modelname]/[number]
+methods = ['GET']
+
+manager = APIManager(app, session=db.session)
+manager.create_api(Book, methods=methods)
+manager.create_api(Country, methods=methods)
 
 @app.route("/")
 def hello_world():
     return '<img src="https://i.kym-cdn.com/photos/images/original/001/211/814/a1c.jpg" alt="cowboy" />'
+
+@app.route("/api")
+def api_overview():
+    return "<h2>BookRUs API</h2><p>This is a (blank) landing page for BookRUs's API. GET requests can be made to our available models.</p>"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
