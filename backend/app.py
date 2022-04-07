f43e79abdfc528ce71feb7cd3977ef2c5d186767
Matthew Escobar <matthew.escobar@me.com>
2022-04-07 14:45:25 -0500

    remade api calls on the backend

--- backend/app.py
+++ backend/app.py
@@ -1,98 +1,93 @@
-from flask import Flask
+from models import app, db, Country, Author, Book
+from schemas import country_schema, author_schema, book_schema
+from flask import jsonify
 from flask_restless import APIManager
-from flask_sqlalchemy import SQLAlchemy
-import data.credentials
-
-# from data.countries import Country
-# from data.books import Book
-# from data.authors import Author
-
-app = Flask(__name__)
-app.debug = True
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.config["SQLALCHEMY_DATABASE_URI"] = data.credentials.db_login
-db = SQLAlchemy(app)
-
-# Define Book table/data model
-class Book(db.Model):
-    # book_id = db.Column(db.String())
-    book_id = db.Column(db.Integer, primary_key=True)
-    book_title = db.Column(db.String())
-    book_author = db.Column(db.String())
-    book_author_id = db.Column(db.Integer)
-    book_published = db.Column(db.String())
-    book_pages = db.Column(db.Integer)
-    book_maturity = db.Column(db.String())
-    book_language = db.Column(db.String())
-    book_categories = db.Column(db.String())
-    book_description = db.Column(db.String())
-    book_image = db.Column(db.String())
-    book_country_id = db.Column(db.Integer)
-
-
-# Define Country table/data model
-class Country(db.Model):
-    country_id = db.Column(db.Integer, primary_key=True)
-    country_name = db.Column(db.String())
-    country_region = db.Column(db.String())
-    country_capital_city = db.Column(db.String())
-    country_lat = db.Column(db.Float)
-    country_long = db.Column(db.Float)
-    country_demonym = db.Column(db.String())
-    country_image = db.Column(db.String())
-    country_authors = db.Column(db.String())
-    country_description = db.Column(db.String())
-    country_languages = db.Column(db.String())
-    country_population = db.Column(db.Integer)
-
-
-# Define Author table/data model
-class Author(db.Model):
-    author_id = db.Column(db.Integer, primary_key=True)
-    author_name = db.Column(db.String())
-    author_birth_date = db.Column(db.String())
-    author_death_date = db.Column(db.String())
-    author_top_work = db.Column(db.String())
-    author_work_count = db.Column(db.Integer)
-    author_bio = db.Column(db.String())
-    author_image = db.Column(db.String())
-    author_country_id = db.Column(db.Integer)
-    author_books = db.Column(db.String())
-    author_genre = db.Column(db.String())
-    author_nationality = db.Column(db.String())
-
 
 # Build database
 db.create_all()
 
-# Method to add CORS headers to all api sessions
-def add_cors_headers(response):
-    response.headers["Access-Control-Allow-Origin"] = "*"
-    return response
+# # Method to add CORS headers to all api sessions
+# def add_cors_headers(response):
+#     response.headers["Access-Control-Allow-Origin"] = "*"
+#     return response
 
+# # Define database API system here, accessible at /api/[modelname]/[number]
+# methods = ["GET"]
 
-# Define database API system here, accessible at /api/[modelname]/[number]
-methods = ["GET"]
-
-manager = APIManager(app, session=db.session)
-book_blueprint = manager.create_api_blueprint(
-    "book", Book, methods=methods, url_prefix="/"
-)
-country_blueprint = manager.create_api_blueprint(
-    "country", Country, methods=methods, url_prefix="/"
-)
-author_blueprint = manager.create_api_blueprint(
-    "author", Author, methods=methods, url_prefix="/"
-)
-blueprints = (book_blueprint, country_blueprint, author_blueprint)
-for blueprint in blueprints:
-    blueprint.after_request(add_cors_headers)
-    app.register_blueprint(blueprint)
-
+# manager = APIManager(app, session=db.session)
+# book_blueprint = manager.create_api_blueprint(
+#     "book", Book, methods=methods, url_prefix="/"
+# )
+# country_blueprint = manager.create_api_blueprint(
+#     "country", Country, methods=methods, url_prefix="/"
+# )
+# author_blueprint = manager.create_api_blueprint(
+#     "author", Author, methods=methods, url_prefix="/"
+# )
+# blueprints = (book_blueprint, country_blueprint, author_blueprint)
+# for blueprint in blueprints:
+#     blueprint.after_request(add_cors_headers)
+#     app.register_blueprint(blueprint)
 
 @app.route("/")
 def hello_world():
     return '<img src="https://i.kym-cdn.com/photos/images/original/001/211/814/a1c.jpg" alt="cowboy" />'
+
+@app.route("/cowboy")
+def hello_world2():
+    return '<img src="https://i.kym-cdn.com/photos/images/original/001/211/814/a1c.jpg" alt="cowboy" />'
+
+@app.route("/country")
+def get_countries():
+    query = db.session.query(Country)
+    result = country_schema.dump(query, many=True)
+
+    return jsonify(
+        {
+            "countries": result
+        }
+    )
+
+@app.route("/country/<int:id>")
+def get_country(id):
+    query = db.session.query(Country).filter_by(country_id=id)
+    result = country_schema.dump(query, many=True)[0]
+    return jsonify(result)
+
+@app.route("/book")
+def get_books():
+    query = db.session.query(Book)
+    result = book_schema.dump(query, many=True)
+
+    return jsonify(
+        {
+            "books": result
+        }
+    )
+
+@app.route("/book/<int:id>")
+def get_book(id):
+    query = db.session.query(Book).filter_by(book_id=id)
+    result = book_schema.dump(query, many=True)[0]
+    return jsonify(result)
+
+
+@app.route("/author")
+def get_authors():
+    query = db.session.query(Author)
+    result = author_schema.dump(query, many=True)
+
+    return jsonify(
+        {
+            "authors": result
+        }
+    )
+
+@app.route("/author/<int:id>")
+def get_author(id):
+    query = db.session.query(Author).filter_by(author_id=id)
+    result = author_schema.dump(query, many=True)[0]
+    return jsonify(result)
 
 
 if __name__ == "__main__":
---
+++ backend/models.py
@@ -0,0 +1,57 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import data.credentials
+
+app = Flask(__name__)
+app.debug = True
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = data.credentials.db_login
+db = SQLAlchemy(app)
+
+# Define Book table/data model
+class Book(db.Model):
+    # book_id = db.Column(db.String())
+    book_id = db.Column(db.Integer, primary_key=True)
+    book_title = db.Column(db.String())
+    book_author = db.Column(db.String())
+    book_author_id = db.Column(db.Integer)
+    book_published = db.Column(db.String())
+    book_pages = db.Column(db.Integer)
+    book_maturity = db.Column(db.String())
+    book_language = db.Column(db.String())
+    book_categories = db.Column(db.String())
+    book_description = db.Column(db.String())
+    book_image = db.Column(db.String())
+    book_country_id = db.Column(db.Integer)
+
+
+# Define Country table/data model
+class Country(db.Model):
+    country_id = db.Column(db.Integer, primary_key=True)
+    country_name = db.Column(db.String())
+    country_region = db.Column(db.String())
+    country_capital_city = db.Column(db.String())
+    country_lat = db.Column(db.Float)
+    country_long = db.Column(db.Float)
+    country_demonym = db.Column(db.String())
+    country_image = db.Column(db.String())
+    country_authors = db.Column(db.String())
+    country_description = db.Column(db.String())
+    country_languages = db.Column(db.String())
+    country_population = db.Column(db.Integer)
+
+
+# Define Author table/data model
+class Author(db.Model):
+    author_id = db.Column(db.Integer, primary_key=True)
+    author_name = db.Column(db.String())
+    author_birth_date = db.Column(db.String())
+    author_death_date = db.Column(db.String())
+    author_top_work = db.Column(db.String())
+    author_work_count = db.Column(db.Integer)
+    author_bio = db.Column(db.String())
+    author_image = db.Column(db.String())
+    author_country_id = db.Column(db.Integer)
+    author_books = db.Column(db.String())
+    author_genre = db.Column(db.String())
+    author_nationality = db.Column(db.String())
