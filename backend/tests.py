ed46efabc2ddfc5f5fac4cb4d806be66767e5be7
William Eng <williame007@gmail.com>
2022-04-13 23:34:09 -0500

    Fix backend unit tests to use data property

--- backend/tests.py
+++ backend/tests.py
@@ -11,42 +11,42 @@ class TestFlaskBackend(unittest.TestCase):
         with self.client:
             response = self.client.get("/authors")
             self.assertEqual(response.status_code, 200)
-            res_json = json.loads(response.text)
+            res_json = json.loads(response.data)
             self.assertEqual(len(res_json["data"]), 10)
 
     def testAuthorById(self):
         with self.client:
             response = self.client.get("/authors/1")
             self.assertEqual(response.status_code, 200)
-            res_json = json.loads(response.text)
+            res_json = json.loads(response.data)
             self.assertEqual(res_json["author_id"], 1)
 
     def testCountryAll(self):
         with self.client:
             response = self.client.get("/countries")
             self.assertEqual(response.status_code, 200)
-            res_json = json.loads(response.text)
+            res_json = json.loads(response.data)
             self.assertEqual(len(res_json["data"]), 10)
 
     def testCountryById(self):
         with self.client:
             response = self.client.get("/countries/1")
             self.assertEqual(response.status_code, 200)
-            res_json = json.loads(response.text)
+            res_json = json.loads(response.data)
             self.assertEqual(res_json["country_id"], 1)
 
     def testBookAll(self):
         with self.client:
             response = self.client.get("/books")
             self.assertEqual(response.status_code, 200)
-            res_json = json.loads(response.text)
+            res_json = json.loads(response.data)
             self.assertEqual(len(res_json["data"]), 10)
 
     def testBookById(self):
         with self.client:
             response = self.client.get("/books/1")
             self.assertEqual(response.status_code, 200)
-            res_json = json.loads(response.text)
+            res_json = json.loads(response.data)
             self.assertEqual(res_json["book_id"], 1)
 
 
